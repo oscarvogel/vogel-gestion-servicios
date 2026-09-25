04cd53038ae8396de32d38dc37b21dca6670a658
Oscar Vogel <oscarvogel@gmail.com>
2026-09-25 18:17:11 -0300

    feat(models): register equipment categories

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 from app.models.company import Company
-from app.models.customer import Customer, Equipment
+from app.models.customer import Customer, Equipment, EquipmentCategory
 from app.models.role import (
     CompanyUserRole,
     Permission,
@@ -12,6 +12,7 @@ __all__ = [
     "Company",
     "Customer",
     "Equipment",
+    "EquipmentCategory",
     "CompanyUser",
     "CompanyUserRole",
     "Permission",
